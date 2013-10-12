cb0c97773513124c2f3d84c6fd36a8d49b3f35e9
CyaNn74 <CyaNn74@gmail.com>
2013-10-13 00:53:26 +0200

    LGPLv3.0 License

--- src/DiscreteReact.java
+++ src/DiscreteReact.java
@@ -1,3 +1,19 @@
+/*
+This file was developed by Yann Caron in october 2013.
+This file is part of Java.react.
+
+Java.react is free software: you can redistribute it and/or modify
+it under the terms of the GNU Less General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Java.react is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Less General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Java.react.  If not, see <http://www.gnu.org/licenses/lgpl.html>.*/
 
 import fr.cyann.functor.Function;
 import fr.cyann.functor.Function1;
@@ -6,14 +22,6 @@ import fr.cyann.functor.Procedure1;
 import fr.cyann.react.Var;
 import fr.cyann.react.VarReact;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author caronyn
- */
 public class DiscreteReact {
 
 	/**
@@ -31,7 +39,7 @@ public class DiscreteReact {
 			}
 		}, a, b);
 
-		sum.map(new Function1<String, Integer> () {
+		sum.map(new Function1<String, Integer>() {
 
 			@Override
 			public String invoke(Integer value) {
@@ -57,7 +65,7 @@ public class DiscreteReact {
 			public boolean invoke(Integer value) {
 				return (value <= 10);
 			}
-		}).map(new Function1<String, Integer> () {
+		}).map(new Function1<String, Integer>() {
 
 			@Override
 			public String invoke(Integer value) {
--- src/fr/cyann/react/TimeReact.java
+++ src/fr/cyann/react/TimeReact.java
@@ -1,7 +1,20 @@
 /*
- * Copyright (C) 2013 CyaNn
- * License modality not yet defined.
- */
+This file was developed by Yann Caron in october 2013.
+This file is part of Java.react.
+
+Java.react is free software: you can redistribute it and/or modify
+it under the terms of the GNU Less General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Java.react is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Less General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Java.react.  If not, see <http://www.gnu.org/licenses/lgpl.html>.*/
+
 package fr.cyann.react;
 
 import fr.cyann.react.data.TimeEvent;
